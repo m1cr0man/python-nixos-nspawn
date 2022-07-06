00f8b2b20ec970cf5bff96647b1f11481ea63c50
Lucas Savva <lucas@m1cr0man.com>
2022-07-06 15:04:17 +0100

    Add overlay to flake output

--- flake.nix
+++ flake.nix
@@ -1,66 +1,82 @@
 {
-    inputs = {
-        nixpkgs.url = "nixpkgs";
-        poetry2nix = {
-            url = "github:nix-community/poetry2nix";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, poetry2nix, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
-        name = "nixos-nspawn";
-        version = builtins.readFile "${self}/nixos_nspawn/version.txt";
-        python = pkgs.python310;
+  outputs = { self, nixpkgs, poetry2nix, flake-utils }:
+    let
+      name = "nixos-nspawn";
+      version = builtins.readFile "${self}/nixos_nspawn/version.txt";
+      pythonVersion = "python310";
 
-        projectDir = self;
+      projectDir = self;
 
+      customOverrides = (pyself: pysuper: {
+        # flake8-annotations is missing poetry-core when parsed by poetry2nix
+        flake8-annotations = pysuper.flake8-annotations.overridePythonAttrs (oldAttrs: {
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pysuper.poetry-core ];
+        });
+      });
+    in
+    {
+      overlays = {
+        default = nixpkgs.lib.composeManyExtensions [
+          poetry2nix.overlay
+          self.overlays."${name}"
+        ];
+        "${name}" = (final: prev: {
+          "${name}" = prev.poetry2nix.mkPoetryApplication {
+            inherit projectDir;
+            python = prev."${pythonVersion}";
+            overrides = prev.poetry2nix.overrides.withDefaults customOverrides;
+            # Skip installing dev-dependencies
+            # https://github.com/nix-community/poetry2nix/issues/47
+            # doCheck = false;
+            meta = {
+              name = "${name}-${version}";
+            };
+          };
+        });
+      };
+
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = import "${nixpkgs}/pkgs/top-level/default.nix" {
-            localSystem.system = system;
-            overlays = [ poetry2nix.overlay ];
-            config = {};
+          localSystem.system = system;
+          overlays = [ self.overlays.default ];
+          config = { };
         };
 
-        # self & super refers to poetry2nix
-        p2n = pkgs.poetry2nix.overrideScope' (self: super: {
-            # pyself & pysuper refers to python packages
-            defaultPoetryOverrides = super.defaultPoetryOverrides.extend (pyself: pysuper: {
-                # flake8-annotations is missing poetry-core when parsed by poetry2nix
-                flake8-annotations = pysuper.flake8-annotations.overridePythonAttrs (oldAttrs: {
-                    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pysuper.poetry-core ];
-                });
-            });
-        });
-    in rec {
+        python = pkgs."${pythonVersion}";
+      in
+      rec {
         packages = {
-            default = p2n.mkPoetryApplication {
-                inherit python projectDir;
-                # Skip installing dev-dependencies
-                # https://github.com/nix-community/poetry2nix/issues/47
-                # doCheck = false;
-                meta = {
-                    name = "${name}-${version}";
-                };
-            };
-            "${name}" = packages.default;
-            "${name}-venv" = p2n.mkPoetryEnv {
-                inherit python projectDir;
-            };
+          default = pkgs."${name}";
+          "${name}" = packages.default;
+          "${name}-venv" = pkgs.poetry2nix.mkPoetryEnv {
+            inherit python projectDir;
+            overrides = pkgs.poetry2nix.overrides.withDefaults customOverrides;
+          };
         };
 
         apps = {
-            default = flake-utils.lib.mkApp { drv = packages.default; };
-            "${name}" = apps.default;
+          default = flake-utils.lib.mkApp { drv = packages.default; };
+          "${name}" = apps.default;
         };
 
         devShells = {
-            default = packages."${name}-venv".env;
-            poetry = (python.withPackages (pyPkgs: [ pyPkgs.poetry ])).env;
+          default = packages."${name}-venv".env;
+          poetry = (python.withPackages (pyPkgs: [ pyPkgs.poetry ])).env;
         };
 
         # Nix < 2.7 compatibility
         defaultPackage = packages.default;
         defaultApp = apps.default;
         devShell = devShells.default;
-    });
+      }));
 }
