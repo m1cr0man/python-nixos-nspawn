f23f06bdc48fc21c247dcaa02f63bb5b835f5c46
Lucas Savva <lucas@m1cr0man.com>
2026-03-29 00:44:31 +0000

    flake/example: Bump python package

--- flake.nix
+++ flake.nix
@@ -106,7 +106,7 @@
           modules = [
             ({ pkgs, ... }: {
               system.stateVersion = "25.11";
-              environment.systemPackages = [ pkgs.python311 ];
+              environment.systemPackages = [ pkgs.python314 ];
               nixosContainer.network.v4.addrPool = [ "10.151.1.1/24" ];
               nixosContainer.forwardPorts = [{ hostPort = 12345; containerPort = 12345; }];
             })
